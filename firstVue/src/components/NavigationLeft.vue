8740c9e941d77a5daead61d26994730d0553f0a9
LiyuHe <18141186450@163.com>
2018-08-23 17:59:26 +0800

    导航侧滑

--- firstVue/src/components/NavigationLeft.vue
+++ firstVue/src/components/NavigationLeft.vue
@@ -1,52 +1,53 @@
 <template>
   <div class="container-fluid">
-
-    <div class="row" v-if="show">
-      <div class="col-md-2 navigation">
-        <ul class="nav nav-tabs nav-stacked navbar-inverse">
-          <li>
-            <a href="#">
-              首页
-            </a>
-          </li>
-          <li>
-            <a href="#systemSetting" class="nav-header collapsed" data-toggle="collapse">
-              系统管理
-              <span class="pull-right glyphicon glyphicon-chevron-down"></span>
-            </a>
-            <ul id="systemSetting" class="nav nav-list collapse secondmenu" style="height: 0px;">
-              <li><a href="mytask"><i class="glyphicon glyphicon-user"></i>用户管理</a></li>
-              <li><a href="#"><i class="glyphicon glyphicon-th-list"></i>菜单管理</a></li>
-              <li><a href="#"><i class="glyphicon glyphicon-asterisk"></i>角色管理</a></li>
-              <li><a href="#"><i class="glyphicon glyphicon-edit"></i>修改密码</a></li>
-              <li><a href="#"><i class="glyphicon glyphicon-eye-open"></i>日志查看</a></li>
-            </ul>
-          </li>
-          <li>
-            <a href="./plans.html">物料管理</a>
-          </li>
-          <li>
-            <a href="./grid.html">分发配置</a>
-          </li>
-          <li>
-            <a href="./charts.html">图表统计</a>
-          </li>
-          <li>
-            <a href="#">关于系统</a>
-          </li>
-        </ul>
+    <transition name="munu">
+      <div class="row munuList" v-if="show">
+        <div class="col-md-2 navigation">
+          <ul class="nav nav-tabs nav-stacked navbar-inverse">
+            <li>
+              <a href="#">
+                首页
+              </a>
+            </li>
+            <li>
+              <a href="#systemSetting" class="nav-header collapsed" data-toggle="collapse">
+                系统管理
+                <span class="pull-right glyphicon glyphicon-chevron-down"></span>
+              </a>
+              <ul id="systemSetting" class="nav nav-list collapse secondmenu" style="height: 0px;">
+                <li><a href="mytask"><i class="glyphicon glyphicon-user"></i>用户管理</a></li>
+                <li><a href="#"><i class="glyphicon glyphicon-th-list"></i>菜单管理</a></li>
+                <li><a href="#"><i class="glyphicon glyphicon-asterisk"></i>角色管理</a></li>
+                <li><a href="#"><i class="glyphicon glyphicon-edit"></i>修改密码</a></li>
+                <li><a href="#"><i class="glyphicon glyphicon-eye-open"></i>日志查看</a></li>
+              </ul>
+            </li>
+            <li>
+              <a href="./plans.html">物料管理</a>
+            </li>
+            <li>
+              <a href="./grid.html">分发配置</a>
+            </li>
+            <li>
+              <a href="./charts.html">图表统计</a>
+            </li>
+            <li>
+              <a href="#">关于系统</a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div class="navigationButton">
-       <transition
-       v-on:enter="enter"
-       v-on:leave="leave"
-       v-bind:css="false">
-       <button v-if="show" v-model="show">点我</button>
-       <button v-else>点我</button>
-     </transition>
-   </div>
- </div>
- 
+    </transition>
+    <transition name="leaveMunu">
+      <div v-if="show" class="imgMunuLeave" @click="show = !show">
+      </div>
+    </transition>
+    <transition name="enterMunu">
+      <div v-if="!show" class="imgMunuEnter" @click="show = !show">
+      </div>
+    </transition>
+<!--     <button @click="show = !show">收起</button> -->
+  </div>
 </div>
 </template>
 
@@ -60,19 +61,51 @@
       }
     },
     methods: {
-      enter: function (el, done) {
-
-      }
+      // enter: function (el, done) {
+      // },
+      // leave: function (el, done) {
+      // }
     }
   }
+  var a = function () {
+    var arr = [];
+    var n = 0;
+    var str = "";
+    for (var j = 0; j < 4; j++) {
+      arr[j] = [];
+      for (var k = 0; k < 4; k++){
+        n++;
+        arr[j][k] = n;
+        str = str + "  " + arr[j][k];
+      }
+    }
+    console.log(str);
+  }
+  a();
 </script>
 
 <style scoped>
   .container-fluid {
     position: absolute;
     top: 68px;
-    left: -15px;
     z-index: 10;
+  }
+  .imgMunuLeave {
+    width: 180px;
+    height: 400px;
+    background-color: #222222;
+    position: absolute;
+    margin-left: -15px;
+    top: 0px;
+    z-index: -1;
+  }
+  .imgMunuEnter {
+    width: 20px;
+    height: 400px;
+    background-color: #222222;
+    position: absolute;
+    margin-left: -15px;
+    z-index: -1;
   }
   .navigation {
     width: 160px;
@@ -82,8 +115,63 @@
     background-color: #716767;
   }
   .navigationButton {
-    margin-left: 160px;
-    height: 100%;
+    float: left;
   }
+  .munuList {
+    z-index: 10;
+    float: left;
+  }
+
+  /*导航栏列表   过渡*/
+  .munu-enter-active {
+    transition: all 1.5s linear;
+    transition-delay: 2s;
+  }
+  .munu-leave-active {
+    transition: all 0.4s;
+    transition-delay: 0.3s;
+  }
+  .munu-enter, .munu-leave-to {
+    opacity: 0;
+  }
+  /*导航栏背景动画*/
+  /*消失*/
+  .leaveMunu-enter-active {
+    animation: leaveMunuLeave 3s;
+  }
+  .leaveMunu-leave-active {
+    animation: leaveMunuEnter 3s;
+  }
+  .leaveMunu-enter, .leaveMunu-leave-to {
+    opacity: 0;
+  }
+  @keyframes leaveMunuLeave {
+    0% {width: 0px; opacity: 0}
+    100% {width: 180px; opacity: 1}
+  }
+  @keyframes leaveMunuEnter {
+    0% {width: 180px; opacity: 1}
+    100% {width: 0px; opacity: 0}
+  }
+  /*导航载入*/
+  .enterMunu-enter-active {
+    animation: enterMunuEnter 0.1s;
+    /*animation-delay: 3s;*/
+  }
+  .enterMunu-leave-active {
+    animation: enterMunuLeave 0.1s;
+  }
+  .enterMunu-enter, .enterMunu-leave-to {
+    opacity: 0;
+  }
+  @keyframes enterMunuEnter {
+    0% {width: 0px; opacity: 0}
+    100% {width: 20px; opacity: 1}
+  }
+  @keyframes enterMunuLeave {
+    0% {width: 20px; opacity: 1}
+    100% {width: 0px; opacity: 0}
+  }
+
 </style>
 
